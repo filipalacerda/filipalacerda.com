e81a627a15e79000733055ee6bcbe02d83975223
Filipa Lacerda <filipalacerda@users.noreply.github.com>
2024-07-17 16:08:16 +0100

    Revert "Use emotion styles"

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    colors: {
+      darkBlue: "#264653",
+      green: "#2A9D8F",
+      yellow: "#E9C46A",
+      orangeLight: "#F4A261",
+      orangeDark: "#E76F51",
+    },
+  },
+  plugins: [],
+};
+export default config;
