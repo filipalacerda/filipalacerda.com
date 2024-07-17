519491e44ab93d0367adfc27713d8b7c599fb7b7
Filipa Lacerda <lacerda.filipa@gmail.com>
2024-07-17 16:48:26 +0100

    Loads profile picture

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+import ProfilePicture from "./images/filipa.jpg";
+
 export default function Home() {
   return (
     <div>
-      <section className="content">Home page</section>
+      <section className="content">
+        Home page
+        <Image
+          src={ProfilePicture}
+          width={200}
+          height={200}
+          alt="Profile picture"
+        />
+      </section>
     </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    loader: "custom",
+    loaderFile: "./loadImage.ts",
+  },
 };
 
 export default nextConfig;
